76f3e963f6a7ac386ac840d27f05db57df81289a
andreas.marneris <andreas.marneris@kidsloop.live>
2022-05-27 16:29:04 +0100

    feat: adding tfe workspace helm chart

--- apifactory/uk/tools/modules/subscriptions-terraform-svc/argo-cd.tf
+++ apifactory/uk/tools/modules/subscriptions-terraform-svc/argo-cd.tf
@@ -134,3 +134,50 @@ resource "argocd_application" "tfcloud-operator" {
     }
   }
 }
+
+resource "argocd_application" "workspace-random" {
+  metadata {
+    name      = "tfe-workspace-random-${local.name_suffix}"
+    namespace = var.argocd_namespace
+    labels = {
+      region = var.region
+      env    = var.project_environment
+      loc    = var.project_region
+      owner  = var.service_owner
+      layer  = "infra"
+    }
+  }
+  wait = true
+
+  spec {
+    project = var.argocd_project
+    source {
+      repo_url        = var.helm_chart_url
+      path            = "${local.project_environment}/${local.project_region}/infrastructure/workspace"
+      target_revision = "main"
+      helm {
+        value_files = ["values.yaml"]
+      }
+    }
+
+    destination {
+      server    = var.kubernetes_server_url
+      namespace = var.product_namespace
+    }
+
+    sync_policy {
+      automated = {
+        prune       = true
+        self_heal   = true
+        allow_empty = false
+      }
+      retry {
+        backoff = {
+          duration     = ""
+          max_duration = ""
+        }
+        limit = "0"
+      }
+    }
+  }
+}
