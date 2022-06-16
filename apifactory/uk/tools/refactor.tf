985d9f64aed55fe5d40e7f7b142f398d8fd19d7e
andreas.marneris <andreas.marneris@kidsloop.live>
2022-06-16 12:15:18 +0100

    revert: backed out refactor tf file because it cannot refer to external modules

--- apifactory/uk/tools/refactor.tf
+++
@@ -1,8 +0,0 @@
-moved {
-  from = module.service_instance.argocd_application.istio
-  to   = module.service_instance.argocd_application.applications["istio-config"]
-}
-moved {
-  from = module.service_instance.argocd_application.microgateway
-  to   = module.service_instance.argocd_application.applications["microgateway"]
-}
